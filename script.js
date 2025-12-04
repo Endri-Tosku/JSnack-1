33f4f0e5fbe91f5a65b52f645ed8fc358792088e
Endri-Tosku <endritosku001@gmail.com>
2025-12-04 12:02:34 +0100

    feat: esempio con tre numeri

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const number1 = Number(prompt("inserisci il primo numero:"))
+/* const number1 = Number(prompt("inserisci il primo numero:"))
 const number2 = Number(prompt("inserisci il secondo numero:"))
 
 if (number1 > number2) {
@@ -7,4 +7,23 @@ if (number1 > number2) {
     console.log("il numero maggiore è", number2);
 } else {
     console.log("il numeri sono uguali");
-}
+} */
+
+
+
+const number1 = Number(prompt("primo numero"))
+const number2 = Number(prompt("secondo numero"))
+const number3 = Number(prompt("terzo numero"))
+
+if (number1 > number2 && number1 > number3) {
+    console.log("il numero maggiore è", number1);
+
+} else if (number2 > number1 && number2 > number3) {
+    console.log("il numero maggiore è", number2);
+
+} else {
+    console.log("il numero maggiore è", number3);
+} 
+
+
+
